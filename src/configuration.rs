5c876edbb68fafb9bc8409fd538af40e5283fb34
Tengiz Sharafiev <btolfa@gmail.com>
2022-06-18 20:16:04 +0300

    Add configuration impl

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,1 +1,23 @@
+use config::{Config, File};
 
+#[derive(serde::Deserialize)]
+pub struct Settings {
+    pub database: DatabaseSettings,
+    pub port: u16,
+}
+
+#[derive(serde::Deserialize)]
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+    pub database_name: String,
+}
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    let s = Config::builder()
+        .add_source(File::with_name("configuration"))
+        .build()?;
+    s.try_deserialize()
+}
